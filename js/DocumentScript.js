35e83bf54282fc8aba00f534727afac7dcd7a291
Russell Baylis <russell.baylis@shopify.com>
2016-09-28 21:44:40 -0400

    Add accessibility features V1

--- js/DocumentScript.js
+++ js/DocumentScript.js
@@ -1,3 +1,9 @@
 function mdl(){
+  if(!document.body.classList.contains('mdl-visible')){
+    document.getElementsByClassName("close")[0].focus();
+  }else{
+    document.getElementsByClassName("mdl-launch")[0].focus();
+  }
 	document.body.classList.toggle('mdl-visible');
 }
+
